c1b7e5c4a9467de71e7991e6feb49b2c4af67ef8
lxxkpad <106137685+lxxkpad@users.noreply.github.com>
2022-06-03 10:56:37 +0700

    Update models.py

--- drugfind/models.py
+++ drugfind/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class SaveImage(models.Model):
-    Image_file = models.ImageField(upload_to = 'upload/')
-
-    #ขั้นตอนเกี่ยวกับการเปลี่ยนขนาดภาพ ก็ใช้หลักการเดิมทั้งหมด
-    file_format = 'JPEG'
-    file_name = 'sample.jpg'
-    content_type = 'image/jpeg'
